ea97e6a43892fcb1a0949e99f9a3792ffd916d40
Aadit Antony <aaditantony1112@gmail.com>
2025-04-23 21:08:25 +0530

    add the code using the statemanagement

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
-import 'package:animations/animations.dart';
 import 'package:flutter/material.dart';
+import 'package:animations/animations.dart';
 
 void main() => runApp(const MyApp());
 
@@ -10,21 +10,41 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: Center(child: Text("tap the button bellow ")),
-        floatingActionButton: OpenContainer(
-          transitionDuration: const Duration(milliseconds: 800),
-          transitionType: ContainerTransitionType.fadeThrough,
-          closedColor: Colors.white, // FAB color
-          openColor: Colors.white, // Screen background
-          closedBuilder:
-              (_, openContainer) => FloatingActionButton(
-                backgroundColor: Colors.purple,
-                onPressed: openContainer,
-                child: const Icon(Icons.email, color: Colors.white),
-              ),
-          openBuilder: (_, __) => const ComposeScreen(),
-        ),
+        body: Center(child: Text("Tap the button below!")),
+        floatingActionButton: const AnimatedFab(), // Using our stateful widget
       ),
+    );
+  }
+}
+
+class AnimatedFab extends StatefulWidget {
+  const AnimatedFab({super.key});
+
+  @override
+  State<AnimatedFab> createState() => _AnimatedFabState();
+}
+
+class _AnimatedFabState extends State<AnimatedFab> {
+  bool _isOpen = false; // Tracks if container is open
+
+  @override
+  Widget build(BuildContext context) {
+    return OpenContainer(
+      closedBuilder:
+          (_, openContainer) => FloatingActionButton(
+            onPressed: () {
+              openContainer(); // Open the container
+              setState(() => _isOpen = true); // Update state
+            },
+            child: Icon(_isOpen ? Icons.send : Icons.email),
+          ),
+      openBuilder: (_, __) {
+        // Reset state when closed
+        WidgetsBinding.instance.addPostFrameCallback((_) {
+          if (mounted) setState(() => _isOpen = false);
+        });
+        return const ComposeScreen();
+      },
     );
   }
 }
